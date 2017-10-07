10c6cf00d225e6c728442cc538c8994628d0d9ff
Camille Ruppli <camille.ruppli@telecom-paristech.fr>
2017-10-07 20:03:34 +0200

    test of the kmeans method with 4 classes

---
+++ kmeans.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+
+T=["Rx1anNormale","Rx3ansFoyerInfGauche","Rx3ansNormaleFace","Rx3ansNormaleProfil","Rx4_5ansPneumopathieNecrosanteDroite","Rx4ansNormale","Rx8_5ansNormale-distension","Rx11ansNormale-sdbronchique"]
+
+for i in range(len(T)):
+    img = cv2.imread('Lungsonly/lungsonly_'+T[i]+'.jpg')
+    Z = img.reshape((-1,3))
+
+    # convert to np.float32
+    Z = np.float32(Z)
+    # define criteria, number of clusters(K) and apply kmeans()
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 4
+    ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+    # Now convert back into uint8, and make original image
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res2 = res.reshape((img.shape))
+    cv2.imwrite('Kmeans/kmeans4_'+T[i]+'.jpg',res2)
