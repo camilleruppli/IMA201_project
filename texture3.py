b9bbbc152c8d4347f60534e66ff1e6e1a2616163
rpbaptista <ree_baptista@me.com>
2017-11-05 19:24:00 +0100

    Add files via upload

--- texture3.py
+++ texture3.py
@@ -11,9 +11,9 @@ from skimage import measure
 
 
 T=["Rx1anNormale","Rx3ansFoyerInfGauche","Rx3ansNormaleFace","Rx3ansNormaleProfil","Rx4_5ansPneumopathieNecrosanteDroite","Rx4ansNormale","Rx8_5ansNormale-distension","Rx11ansNormale-sdbronchique"]
-SIZE_GRID = (128,128)
-
-for i in range(len(T)):
+SIZE_GRID = (10,10)
+bins = range (0,255)
+for i in range(1):
     im=cv2.imread('Lungsonly/lungsonly_'+T[i]+'.jpg', 0)
 
     rows=im.shape[0]
@@ -30,14 +30,13 @@ for i in range(len(T)):
             aux = list(range(SIZE_GRID[0]))
             aux = np.array(aux)
             piece = im[aux*(row_index+1),:][:,aux*(col_index+1)]
-            print "Comparacao"
-            print im[158,:], im[158][:]
-            print im[159,:], im[159][:]
-            print piece
-            #if (np.all(piece, np.zeros(SIZE_GRID))!=0):
-            #print "ei"
-            histo = np.histogram(piece)
-            #cv2.imwrite("histo/histo_"+"col"+str(col_index)+"_"+"row"+str(row_index)+"_"+T[i]+".jpg",histo)
-            #allHisto[row_index,col_index] = histo
-            mpl.pyplot.hist(histo, bins='auto')
-            mpl.pyplot.savefig("histo/histo_"+T[i]+"col_index"+str(col_index)+"_"+"row_index"+str(row_index)+".jpg")
+            piece = np.round(piece)
+            piece = np.squeeze(np.asarray(piece))
+            print piece.max()
+            if (piece.mean()>=10):
+                print "entrei"
+                histo = np.histogram(piece,bins)
+                print histo[0]
+                mpl.pyplot.plot(histo[0])
+                mpl.pyplot.savefig("histo/histo_"+T[i]+"col_index"+str(col_index)+"_"+"row_index"+str(row_index)+".jpg")
+                mpl.pyplot.close('all')
