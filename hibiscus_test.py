4c2e8e0ac6bf863fb0803f68982dcb65e10d6aea
Camille Ruppli <camille.ruppli@telecom-paristech.fr>
2017-11-05 17:02:03 +0100

    change with the circular convolution with a blurring gaussian kernel

--- hibiscus_test.py
+++ hibiscus_test.py
@@ -1,22 +1,26 @@
 import cv2
 import numpy as np
 import random as rd
+import scipy.ndimage
 
 im=cv2.imread('index_01.png',0)
 im1=im.copy()
 im2=im.copy()
 gradf=np.gradient(im1)
+
 rows = im.shape[1]
 columns = im.shape[0]
-print(rows)
-print(columns)
+sigma=0.1
 for i in range(rows):
     for j in range(columns):
         dfx=gradf[0][j][i]
         dfy=gradf[1][j][i]
         V=np.array([dfx,dfy])
         tensor=np.tensordot(V,V,0)
-        vp=np.linalg.eig(np.matrix(tensor))
+        #print(tensor)
+        kernel = 1 / (2 * np.pi * sigma ** 2) * np.array([[1, np.exp(-1 / (2 * sigma ** 2))], [np.exp(-1 / (2 * sigma ** 2)), 1]])
+        T = np.real(np.fft.ifft( np.fft.fft(tensor)*np.fft.fft(kernel) )) #circular convolution
+        vp=np.linalg.eig(np.matrix(T))
         v1=vp[0][0]
         v2=vp[0][1]
         print(v1,v2)
