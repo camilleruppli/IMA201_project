63be45d3af50647e90c7e8a4eb5ebda60c53e76c
Camille Ruppli <camille.ruppli@telecom-paristech.fr>
2017-11-06 22:47:45 +0100

    Code and histograms to see that we cannot apply the canny hysteresis thresholding

--- Histograms/histogram.py
+++ Histograms/histogram.py
@@ -2,19 +2,19 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-root = "/Users/camilleruppli/Desktop/Telecom/IMA/IMA201/"
-T=["Rx1anNormale","Rx3ansFoyerInfGauche","Rx3ansNormaleFace","Rx3ansNormaleProfil","Rx4,5ansPneumopathieNecrosanteDroite","Rx4ansNormale","Rx8,5ansNormale-distension","Rx11ansNormale-sdbronchique"]
+#root = "/Users/camilleruppli/Desktop/Telecom/IMA/IMA201/"
+T=["Rx1anNormale","Rx3ansFoyerInfGauche","Rx3ansNormaleFace","Rx3ansNormaleProfil","Rx4_5ansPneumopathieNecrosanteDroite","Rx4ansNormale","Rx8_5ansNormale-distension","Rx11ansNormale-sdbronchique"]
 
 
 for i in range(len(T)):
-    img = cv2.imread(root+T[i]+'.jpg')
+    img = cv2.imread(T[i]+'.jpg')
     color = ('b','g','r')
     for j,col in enumerate(color):
         histr = cv2.calcHist([img],[j],None,[256],[0,256])
         plt.plot(histr,color = col)
         plt.xlim([0,256])
         #plt.show() doen't work on my computer anymore
-        plt.savefig(root+'histo'+T[i]+'.png')
+        plt.savefig('Histograms/histo_'+T[i]+'.png')
         plt.close()
 
 
